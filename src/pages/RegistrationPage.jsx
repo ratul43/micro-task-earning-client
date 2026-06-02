1a4f15a2c6ebe34fc39b9869111345cf6d73bedf
ratul43 <kmratul971@gmail.com>
2026-06-02 11:54:57 +0600

    Profile pic field modify in Registration page

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -161,7 +161,7 @@ const RegistrationPage = () => {
                   reader.onload = () => setProfileImagePreview(reader.result);
                   reader.readAsDataURL(file);
                 }}
-                className="w-full"
+                className="w-full border px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
               {profileImagePreview && (
                 <img
