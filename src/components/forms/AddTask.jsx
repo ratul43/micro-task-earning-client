587052e0c26519862575b489e1bf059b202f5eed
ratul43 <kmratul971@gmail.com>
2026-06-02 11:07:44 +0600

    Image upload option in add task form

--- src/components/forms/AddTask.jsx
+++ src/components/forms/AddTask.jsx
@@ -163,16 +163,41 @@ const AddTask = () => {
 
         {/* Image URL */}
         <div>
-          <label className="block font-medium mb-1">Image URL</label>
-          <input
-            type="text"
-            placeholder="Task Image URL"
-            {...register("task_image_url", { required: "Image URL is required" })}
-            className="w-full border p-2 rounded"
-          />
-          {errors.task_image_url && (
-            <p className="text-red-500 text-sm">{errors.task_image_url.message}</p>
-          )}
+          <label className="block font-medium mb-1">Image</label>
+         
+        </div>
+        
+        <div>
+<>
+ <div className="max-w-xl">
+  <label className="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+    <span className="flex items-center space-x-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-6 h-6 text-gray-600"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+        />
+      </svg>
+      <span className="font-medium text-gray-600">
+        Drop files to Attach, or
+        <span className="text-blue-600 ml-1 underline">browse</span>
+      </span>
+    </span>
+    <input type="file" name="file_upload" className="hidden" />
+  </label>
+</div>
+
+
+</>
+
         </div>
 
         {/* Total Cost Display */}
